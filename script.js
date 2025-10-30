5e43d268d47d316564f62c80841620af4d1a9f98
Ben Ford <ben4ord@hotmail.com>
2025-10-30 11:59:25 -0600

    Updated reservation page and styling

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+// toggle links off on on for mobile or small screened devices
 function toggleMenu() {
   const menu = document.querySelector('.center-links');
   console.log(menu);
@@ -6,6 +7,7 @@ function toggleMenu() {
 }
 
 // Wait for page to load before getting elements
+// for slide show on homepage 
 window.addEventListener("DOMContentLoaded", () => {
   let slideIndex = 0;
   const slide = document.getElementById("slidesImg");
@@ -31,3 +33,45 @@ window.addEventListener("DOMContentLoaded", () => {
     })
     .catch(err => console.error("Error Loading image:", err));
 });
+
+// Wait for page to load before getting elements
+// for slide Reservation slide show
+let slide ;
+let resSlideIndex = 0;
+let images;
+
+window.addEventListener("DOMContentLoaded", () => {
+
+  slide = document.getElementById("resSlidesImg");
+  // Next/previous controls
+  fetch('reservationImages.json')
+    .then(response => response.json())
+    .then(data => {
+      images = data.images;
+      console.log(images.length);
+    })
+    .catch(err => console.error("Error Loading image:", err));
+});
+
+function plusSlides(n) {
+  showResSlides(resSlideIndex += n);
+}
+
+
+function showResSlides(n) {
+
+  if (n > images.length){
+    resSlideIndex = 1;
+    console.log("Meow");
+
+  }
+  if (n < 1){
+    resSlideIndex = images.length;
+    console.log("Meow");
+
+  }
+  console.log(resSlideIndex);
+  slide.src = images[resSlideIndex-1];
+
+}
+
